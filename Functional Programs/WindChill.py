0fc93a06220607b6404b1176da5a49c033203c09
ritikapatel1410 <patelrit1410@gmail.com>
2021-02-11 23:53:41 +0530

    windchill solution added

---
+++ Functional Programs/WindChill.py
@@ -0,0 +1,20 @@
+'''
+ @Author: Ritika Patidar
+ @Date: 2021-02-11 18:10:30
+ @Last Modified by: Ritika Patidar
+ @Last Modified time: 2021-02-11 18:10:30 
+ @Title : Wind chill Problem
+'''
+def wind_chill():
+    try:
+        t=float(input("enter temperature less 50 in Fahrenheit: "))
+        v=float(input("enter speed greater then equal to 3 and less then or equal to 120 in miles per hour: "))
+        if(t<=50.0 and (v<=120.0 and v>=3)):
+            w=35.74+0.6215*t+pow((0.4275*t-35.75)*v,0.16)
+            print("wind chill: ", w)
+        else:
+            wind_chill()
+    except:
+        wind_chill()
+wind_chill()
+
