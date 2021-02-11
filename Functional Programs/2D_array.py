9148f45c726085d91022f7a3206219f170971d81
ritikapatel1410 <patelrit1410@gmail.com>
2021-02-11 20:43:36 +0530

    2D array code added

---
+++ Functional Programs/2D_array.py
@@ -0,0 +1,24 @@
+'''
+ @Author: Ritika Patidar
+ @Date: 2021-02-10 17:40:30
+ @Last Modified by: Ritika Patidar
+ @Last Modified time: 2021-02-11 15:07:40 
+ @Title : Create 2D array Problem
+'''
+while True:    
+    try:
+        n=int(input("enter the number of row: "))
+        m=int(input("enter the number of coloumns: "))
+        array=[ [input("enter the value: ") for i in range(m)] for j in range(n)]
+        print("2D array:", array)
+        break
+    except:
+        print("enter only number")
+    try:
+        n=int(input("enter the number of row: "))
+        m=int(input("enter the number of coloumns: "))
+        array=[ [input("enter the value: ") for i in range(m)] for j in range(n)]
+        print("2D array:", array)
+        break
+    except:
+        print("enter only number")
