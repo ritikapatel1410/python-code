fa43962a71cc9852c835d878719f5281cbf920d8
ritikapatel1410 <patelrit1410@gmail.com>
2021-02-11 21:59:25 +0530

    add triplet sum zero count

---
+++ Functional Programs/sum_zero_array.py
@@ -0,0 +1,24 @@
+'''
+ @Author: Ritika Patidar
+ @Date: 2021-02-10 17:50:30
+ @Last Modified by: Ritika Patidar
+ @Last Modified time: 2021-02-11 15:20:40 
+ @Title : count triplet sum zero problem
+'''
+while True:
+    try:
+        n=int(input("enter the number of element: "))
+        array=[int(input("enter the value: ")) for i in range(n)]
+        print("input array is: ", array)
+        count=0
+        for i in range(n-2):
+            for j in range(i+1,n-1):
+                for k in range(j+1,n):
+                    if(array[i]+array[j]+array[k]==0):
+                        count+=1
+        print("triplet sums are: ", count)
+        break
+    except:
+        print("enter only number try again !!")
+
+
