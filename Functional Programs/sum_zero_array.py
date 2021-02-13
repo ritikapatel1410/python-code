02f8bf34295bbd8833c9aa73d82bf38b5e3c2562
patelritika1410 <patelrit1410@gmail.com>
2021-02-13 11:35:31 +0530

    variable name modified

--- Functional Programs/sum_zero_array.py
+++ Functional Programs/sum_zero_array.py
@@ -2,24 +2,24 @@
  @Author: Ritika Patidar
  @Date: 2021-02-10 17:50:30
  @Last Modified by: Ritika Patidar
- @Last Modified time: 2021-02-11 15:20:40 
+ @Last Modified time: 2021-02-13 10:20:40 
  @Title : count triplet sum zero problem
 '''
 while True:
     try:
-        n=int(input("enter the number of element: "))
-        array=[int(input("enter the value: ")) for i in range(n)]
-        print("input array is: ", array)
-        count=0
-        for i in range(n-2):
-            for j in range(i+1,n-1):
-                for k in range(j+1,n):
-                    if(array[i]+array[j]+array[k]==0):
-                        print(array[i],array[j],array[k])
-                        count+=1
-        print("triplet sums are: ", count)
+        user_input=int(input("enter the number of element: "))
+        input_array=[int(input("enter int element of array: ")) for i in range(user_input)]
+        print("input array is: ", input_array)
+        sum_zero_count=0
+        for input_array_index_i in range(user_input-2):
+            for input_array_index_j in range(input_array_index_i+1,user_input-1):
+                for input_array_index_k in range(input_array_index_j+1,user_input):
+                    if(input_array[input_array_index_i]+input_array[input_array_index_j]+input_array[input_array_index_k]==0):
+                        print(input_array[input_array_index_i],input_array[input_array_index_j],input_array[input_array_index_k])
+                        sum_zero_count+=1
+        print("triplet zero sums are: ",sum_zero_count)
         break
-    except:
-        print("enter only number try again !!")
+    except ValueError:
+        print("enter only int number try again !!")
 
 
