c527121f213ce91da849c8229e93a48454be156e
patelritika1410 <patelrit1410@gmail.com>
2021-02-13 10:42:48 +0530

    veriable modified in 2D_array.py

--- Functional Programs/2D_array.py
+++ Functional Programs/2D_array.py
@@ -2,16 +2,16 @@
  @Author: Ritika Patidar
  @Date: 2021-02-10 17:40:30
  @Last Modified by: Ritika Patidar
- @Last Modified time: 2021-02-11 15:07:40 
+ @Last Modified time: 2021-02-13 10:37:40 
  @Title : Create 2D array Problem
 '''
 while True:    
     try:
-        n=int(input("enter the number of row: "))
-        m=int(input("enter the number of coloumns: "))
-        array=[ [input("enter the value: ") for i in range(m)] for j in range(n)]
+        row=int(input("enter the number of row: "))
+        column=int(input("enter the number of coloumns: "))
+        array=[ [input("enter the value: ") for c in range(column)] for r in range(row)]
         print("2D array:", array)
         break
-    except:
-        print("enter only number")
+    except ValueError:
+        print("enter int number only try again!!")
 
