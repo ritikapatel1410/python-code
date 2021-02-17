62de1a37f9948944e42a06c2b3adb0d172bca63d
patelritika1410 <patelrit1410@gmail.com>
2021-02-17 20:25:55 +0530

    Add: Address_Book code added

---
+++ Object-oriented-program/code/Addressbook.py
@@ -0,0 +1,18 @@
+'''
+ @Author: Ritika Patidar
+ @Date: 2021-02-17 12:15:10
+ @Last Modified by: Ritika Patidar
+ @Last Modified time: 2021-02-17 12:15:38  
+ @Title : Address Book problem
+'''
+class add_contact:
+    def __init__(self,first_name,last_names, address, city, state, zip_code, phone_number):
+        self.first_name=first_name
+        self.last_names=last_names
+        self.address=address
+        self.city=city
+        self.state=state
+        self.zip=zip_code
+    def add_detail(self):
+        
+
