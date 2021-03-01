5135f1b240d93c99280730612ec058700cd46765
patelritika1410 <patelrit1410@gmail.com>
2021-03-01 17:58:56 +0530

    Refactor: docstring added

--- Object-oriented-program/Clinique_Management_Programme/code/test/test_patient.py
+++ Object-oriented-program/Clinique_Management_Programme/code/test/test_patient.py
@@ -1,3 +1,10 @@
+'''
+ @Author: Ritika Patidar
+ @Date: 2021-02-22 21:15:10
+ @Last Modified by: Ritika Patidar
+ @Last Modified time: 2021-02-22 21:15:38  
+ @Title : patient program for clinique Management Program
+'''
 import os
 import sys
 import unittest
@@ -7,9 +14,21 @@ import patient
 import json_operation
 
 class PatientTestCase(unittest.TestCase):
+    """
+    Description:
+        This class define for test all case of patient_search method of patient.py file
+    """
     def setUp(self):
         self.data_patient=json_operation.load_data_patient()
     def test_patient(self):
+        """
+        Description:
+            This method define for test all case of patient_search method of patient.py file
+        Parameter:
+            None
+        Return:
+            None
+        """
         result=patient.patient_management().patient_search("name","ananya")
         self.assertEqual(result,"detail searched")
         self.assertIsNotNone(result)
